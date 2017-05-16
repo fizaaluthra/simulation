26cfc6edc2b913b7d42b80079ad46362c534f98d
Fizaa Luthra <fizaaluthra@Fizaas-MBP.lan>
2017-05-16 16:15:20 -0400

    fixed CSS and authentication

--- server.js
+++ server.js
@@ -27,50 +27,89 @@ app.get('/polls', function(req, res, next) {
   return
 });
 app.get('/pollresults', function(req, res, next) {
-  db.collection('polls', function(err, pollsCollection) {
-    var poll = decodeURIComponent(req.query.poll);
-   pollsCollection.findOne({text: poll}, function(err, poll){
-      return res.send(poll);
-    });
-    return
+  db.collection('polls').findOne({text: req.query.poll}, function(err, poll){
+
+    return res.send(poll);
   });
-  return
 });
 app.post('/polls', function(req, res, next){
+  var token = req.headers.authorization;
+  var user = jwt.decode(token, JWT_SECRET);
   db.collection('polls', function(err, pollsCollection){
-    pollsCollection.insert({text: req.body.newPoll,textencoded: req.body.newPollEncoded, options: req.body.options}, {w:1}, function(err){
+    pollsCollection.insert({text: req.body.newPoll,textencoded: req.body.newPollEncoded, options: req.body.options, user: user._id, username: user.username, users_voted: req.body.users_voted}, {w:1}, function(err){
       return res.send();
     });
   });
-  res.send();
+  return res.send();
 });
 
 app.put('/polls/remove', function(req, res, next){
+  var token = req.headers.authorization;
+  var user = jwt.decode(token, JWT_SECRET);
   db.collection('polls', function(err, pollsCollection){
     var pollId = req.body.poll._id;
 
-    pollsCollection.remove({_id: ObjectId(pollId)}, function(err){
+    pollsCollection.remove({_id: ObjectId(pollId), user: user._id}, function(err){
       return res.send();
+    }, function(err){
+      return res.status(400).send();
     });
   });
 
-  res.send();
+  return res.send();
 });
 
+var helper = function(user, req){
+db.collection('polls').findOneAndUpdate({'text':req.body.text}, {
+  $set: {'options': req.body.options},
+  $push:{'users_voted': user.username}
+});
+ //  db.collection('polls', function(err, pollsCollection){
+ //
+ //  pollsCollection.findOneAndUpdate({'text': req.body.text}, { $set: {'options': new_options}, $push: {'users_voted': user.username}}, function(res){
+ //    console.log(req.body.text);
+ //   });
+ // });
+ return
+}
 app.put('/polls/update', function(req, res, next){
+  var token = req.headers.authorization;
+  var user = jwt.decode(token, JWT_SECRET);
+  var flag = 0;
   db.collection('polls', function(err, pollsCollection){
-    var text = req.body.text;
-    var new_options = req.body.options;
-    console.log(new_options);
-    pollsCollection.findOneAndUpdate({'text': text}, { $set: {'options': new_options}}, function(err){
-      return res.send();
-    });
+    var text = decodeURIComponent(req.body.text);
+    var flag = 0;
+    pollsCollection.findOne({'text': text}, function(err,result){
+
+      var flag2 = 0;
+      for (var i = 0; i < result.users_voted.length; ++i){
+        if (result.users_voted[i] === user.username){
+          flag = 1;
+          return res.status(400).send();
+      }
+    }
+    helper(user, req);
+    return res.send();
   });
-  res.send();
+  //   if (flag2 === 0){
+  //
+  //   pollsCollection.findOneAndUpdate({'text': text}, { $set: {'options': new_options}, $push: {'users_voted': user.username}}, function(res){
+  //     return
+  //   });
+  // }
+  // else{
+  //   console.log('hi');
+  //   return res.status(400).send();
+  // }});
+//   });
+// } else{
+//   return res.status(400).send();
+// }
+});
 });
 
 app.post('/users', function(req, res, next){
-  db.collection('users', function(err, usersCollection){
+  db.collection('Users', function(err, usersCollection){
     bcrypt.genSalt(10, function(err, salt){
       bcrypt.hash(req.body.password, salt, function(err, hash){
 
@@ -85,25 +124,27 @@ app.post('/users', function(req, res, next){
       });
     });
   });
-  res.send();
+  return res.send();
 });
 app.put('/users/signin', function(req, res, next){
-  db.collection('users', function(err, usersCollection){
-    usersCollection.findOne({username: req.body.username}, function(err, user){
-      bcrypt.compare(req.body.password, user.password, function(err, result){
-        if(result){
-          var token = jwt.encode(user, JWT_SECRET);
-          return res.json({token: token});
-          return res.send();
+  db.collection('Users').findOne({username: req.body.username}, function(err, user){
+
+    if(user){
+    bcrypt.compare(req.body.password, user.password, function(err, result){
+
+            var token;
+            if(result){
+
+               token = jwt.encode(user, JWT_SECRET);
+            }
+
+              return res.json({token: token});
+          });
         }
-        else{
-          return res.status(400).send();
-        }
-      });
-    });
-  });
-  res.send();
+        return
+});
+return
 });
 app.listen(3000, function(){
-  console.log('Example app');
+
 });
